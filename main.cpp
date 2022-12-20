f659f98e9e686108d1b20b4d6077d2dc56bdecd0
Minhhieu0910 <117256437+Minhhieu0910@users.noreply.github.com>
2022-12-20 21:41:09 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,65 +1,77 @@
 #include <iostream>
-//Cho mảng a, số nguyên M. Tìm 1 mảng con sao cho tổng các phần tử bằng M
+#include <math.h>
+//Tính trung bình nhân các số dương trong ma trận các số thực
 using namespace std;
-void nhapmang(int a[],int n)
+#define Max 100
+void nhapmatran(float a[][Max],int row, int col)
 {
-   cout<<"nhap gia tri cua cac phan tu trong mang:"<<endl;
-   for(int i=0;i<n;i++)
-   {
-       cout<<"a["<<i<<"]= "<<endl;
-       cin>>a[i];
-   }
-}
-void xuatmangcon(int b[],int n)
-{
-    for(int i=0;i<n;i++)
+    for(int i=0;i<row;i++)
     {
-        cout<<b[i]<<"  ";
-    }
-    cout<<endl;
-}
-int tong10(int b[],int n,int M)
-{
-    int m=0;
-    for(int i=0;i<n;i++)
-    {
-        m=b[i]+m;
-    }
-    if(m==M)
-    {
-        return 1;
-    }
-    return 0;
-}
-void tim(int a[],int n,int M)
-{
-    int b[100];
-    for(int i=0;i<n;i++)
-    {
-        for(int y=1;y<=n;y++)
+        for(int j=0;j<col;j++)
         {
-            int nb=0;
-            for(int j=i;j<y;j++)
-            {
-                b[nb]=a[j];
-                nb++;
-            }
-            if(tong10(b,nb,M)==1)
-            {
-                xuatmangcon(b,nb);
-            }
+            cout<<"Nhap gia tri cac phan tu trong ma tran:"<<endl;
+            cout<<"a["<<i<<"]["<<j<<"]= "<<endl;
+            cin>>a[i][j];
         }
     }
 }
+void xuatmatran(float a[][Max],int row,int col)
+{
+    cout<<"Ta co ma tran nhu sau:"<<endl;
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<a[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+}
+void tbnhan(float a[][Max],int &c, int &d)
+{
+    float x=1;
+    int dem=0;
+    for(int i=0;i<c;i++)
+    {
+        for(int j=0;j<d;j++)
+        {
+           if(a[j][i] >0)
+           {
+               x=x*a[j][i];
+               dem++;
+           }
+        }
+    }
+    float y=0;
+    y=pow(x,1.0 /dem);
+    cout<<"Trung binh nhan cac so duong trong ma tran bang = "<<y;
+}
 int main()
 {
-    int a[100];
-    int n,M;
-    cout<<"nhap n:"<<endl;
-    cin>>n;
-    nhapmang(a,n);
-    cout<<"Nhap so nguyen M:"<<endl;
-    cin>>M;
-    tim(a,n,M);
+   float a[Max][Max];
+   int row,col;
+   do
+    {
+         cout<<"Nhap vao so cot cua ma tran:"<<endl;
+         cin>>col;
+         if(col<1 || col >Max)
+         {
+             cout<<"Nhap so cot bi loi, vui long nhap lai !"<<endl;
+         }
+    }while(col<1 || col >Max);
+
+    do
+    {
+        cout<<"Nhap vao so hang cua ma tran:"<<endl;
+         cin>>row;
+          if(row<1 || row >Max)
+         {
+             cout<<"Nhap so cot bi loi, vui long nhap lai !"<<endl;
+         }
+    }while(row<1 || row >Max);
+
+    nhapmatran(a,col,row);
+    xuatmatran(a,col,row);
+    tbnhan(a,col,row);
     return 0;
 }
